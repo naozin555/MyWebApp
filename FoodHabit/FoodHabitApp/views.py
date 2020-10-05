94985e84a55917861866aa88ceaeaceb8fdf34e9
naozin555 <37050583+naozin555@users.noreply.github.com>
2020-10-05 22:30:17 +0900

    Revert "不要なコードの除去"
    This reverts commit 0de4b0d7e05c375026fa3727e00529b04c8cf686.

--- FoodHabit/FoodHabitApp/views.py
+++ FoodHabit/FoodHabitApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import DeleteView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import FoodHabitModel, Board
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
@@ -54,15 +54,20 @@ def _handle_uploaded_file(f):
         for chunk in f.chunks():
             destination.write(chunk)
     # csvデータをDBに登録する
+    print("csvを開けます")
+    print(csv_filepath)
     food_habit_df = pd.read_csv(csv_filepath)
-    food_habit_instances = [FoodHabitModel(
+    print(food_habit_df)
+    for i in (food_habit_df['日付']):
+        print(i)
+    food_habit_instances = [FoodHabitModel.objects.bulk_create(
         date=date,
         weight=weight,
         food_name=food_name,
         food_category=food_category,
-    ) for date, weight, food_name, food_category
-        in zip(food_habit_df['日付'], food_habit_df['体重'],
-               food_habit_df['食品名'], food_habit_df['食品のカテゴリ'])]
+    ) for date, weight, food_name, food_category in zip(food_habit_df['日付'], food_habit_df['体重'],
+                                                        food_habit_df['食品名'], food_habit_df['食品のカテゴリ'])]
+    print(food_habit_instances)
     FoodHabitModel.objects.bulk_create(food_habit_instances)
     os.remove(csv_filepath)  # アップロードしたファイルを削除
 
@@ -109,6 +114,13 @@ def read_func(request, pk):
         post.readtext = post.readtext + '' + post2
         post.save()
         return redirect('list')
+
+
+class FoodHabitCreate(CreateView):
+    template_name = 'create.html'
+    model = Board
+    fields = ("author", "date")
+    success_url = reverse_lazy('list')
 
 
 class FoodHabitDelete(DeleteView):
