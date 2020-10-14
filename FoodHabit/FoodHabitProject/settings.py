9004519da3e56ebe4fcf221c24fcf1b27efced87
naozin555 <37050583+naozin555@users.noreply.github.com>
2020-10-14 16:30:30 +0900

    django_heroku部分のコメントアウト

--- FoodHabit/FoodHabitProject/settings.py
+++ FoodHabit/FoodHabitProject/settings.py
@@ -139,10 +139,10 @@ try:
 except ImportError:
     pass
 
-if not DEBUG:
-    SECRET_KEY = os.environ['SECRET_KEY']
-    import django_heroku
-    django_heroku.settings(locals())
+# if not DEBUG:
+#     SECRET_KEY = os.environ['SECRET_KEY']
+#     import django_heroku
+#     django_heroku.settings(locals())
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
