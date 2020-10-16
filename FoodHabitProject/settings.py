461f54ee5438422c22837540de3e8962c4558203
naozin555 <37050583+naozin555@users.noreply.github.com>
2020-10-16 12:43:55 +0900

    staticfiles=False

--- FoodHabitProject/settings.py
+++ FoodHabitProject/settings.py
@@ -151,4 +151,4 @@ except ImportError:
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
-    django_heroku.settings(locals())
+    django_heroku.settings(locals(), staticfiles=False)
